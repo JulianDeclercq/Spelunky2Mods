95e3c760a132cb9d1b6321fb9ee5425cb9f9284c
Julian Declercq <julian.declercq1@gmail.com>
2023-04-03 00:20:14 +0200

    safe drill - * Only replace chuncks that are in the drill's path * Replace kali altars * Replace dice shops

--- safe drill/main.lua
+++ safe drill/main.lua
@@ -1,6 +1,6 @@
 meta.name = "Safe Drill"
-meta.version = "0.2"
-meta.description = "The drill will always have a clear path, no lava, no shopkeepers and no kali altars."
+meta.version = "1.0"
+meta.description = "The drill will always have a clear path: no lava, no shopkeepers, no kali altars."
 meta.author = "Jools"
 
 local rooms_to_replace = {
@@ -13,41 +13,41 @@ local rooms_to_replace = {
   [71] = "SHOP_ATTIC",
   [72] = "SHOP_ATTIC_LEFT",
   [73] = "SHOP_BASEMENT",
-  [74] = "SHOP_BASEMENT_LEFT"
+  [74] = "SHOP_BASEMENT_LEFT",
+  [75] = "DICESHOP",
+  [76] = "DICESHOP_LEFT",
+  [115] = "ALTAR"
 }
 
-DRILL_PRESENCE = 3
-WATER_ID = 909
-
+DRILL_ZONE = { startX = 0, endX = 0, column = 0}
 is_drill_level = false
 
-SAFE_ZONE = { startX = 0, endX = 0 }
 set_callback(function(room_gen_ctx)
-  is_drill_level = test_flag(state.presence_flags, DRILL_PRESENCE)
+  is_drill_level = test_flag(state.presence_flags, PRESENCE_FLAG.DRILL)
   if not is_drill_level then
     return
   end
 
-  local tiles_margin, room_width, bleed = 2, 10, 1
+  local tiles_margin, room_width, bleed = 2, 10, 1 -- TODO: Test without bleed
   for x = 0, state.width - 1 do
         for y = 0, state.height - 1 do
-
           local room_template_here = get_room_template(x, y, 0)
           --print(F'{x} {y}: {get_room_template_name(room_template_here)}({room_template_here})')
           if room_template_here == ROOM_TEMPLATE.VLAD_DRILL then
-            print(F'found drill at {x}, {y}')
+            print(F'DRILL LEVEL! DRILL AT {x},{y}')
             local startX = tiles_margin + room_width * x - bleed
-            local endX = startX + room_width + bleed
-            print(F'drill spans in coords from {startX} to {endX}')
-            SAFE_ZONE.startX = startX
-            SAFE_ZONE.endX = endX
-            print(F'SAFE_ZONE {inspect(SAFE_ZONE)}')
+            DRILL_ZONE.startX = startX
+            DRILL_ZONE.endX = startX + room_width + bleed
+            DRILL_ZONE.column = x
           end
 
-          local target = rooms_to_replace[room_template_here]
-          if target ~= nil then
-            print(F'REPLACING {target} at {x}, {y}')
-            room_gen_ctx:set_room_template(x, y, 0, ROOM_TEMPLATE.CHUNK_AIR) -- check if this needs to be done in the context
+          -- drill always spawns in highest row, so ignore the first row (y = 0)
+          if y > 0 and x == DRILL_ZONE.column then
+            local target = rooms_to_replace[room_template_here]
+            if target ~= nil then
+              room_gen_ctx:set_room_template(x, y, 0, ROOM_TEMPLATE.CHUNK_AIR) -- check if this needs to be done in the context
+              print(F'REPLACED {target} at {x}, {y}')
+            end
           end
         end
     end  
@@ -58,25 +58,22 @@ set_pre_tile_code_callback(function(x, y, l)
     return
   end
 
-  if x < SAFE_ZONE.startX or x > SAFE_ZONE.endX then
+  if x < DRILL_ZONE.startX or x > DRILL_ZONE.endX then
     return
   end    
 
-  print(F'Replacing lava at {x}, {y}')
-  spawn_liquid(WATER_ID, x, y)
+  --print(F'Replacing lava at {x}, {y}')
+  spawn_liquid(ENT_TYPE.LIQUID_WATER, x, y)
   return true -- prevents the original tile (lava) from being spawned
 end, "lava")
 
 -- REMOVE
 set_callback(function()
   players[1]:give_powerup(ENT_TYPE.ITEM_POWERUP_UDJATEYE)
+  god(true)
 end, ON.LEVEL)
 
 --[[
-
-every chunk is 10? wide (42 total - 2x 2 tiles border) x position is 0 indexed]
-
-13 - half tile width (0.5) is the first x pos of the first block of the 2nd chunk
 
 1. ON.POST_ROOM_GENERATION
 2. ON.PRE_HANDLE_ROOM_TILES
