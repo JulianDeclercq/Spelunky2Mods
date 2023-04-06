fd86134d75e780f7952b650ab3dcc91dfab1ad4b
Julian Declercq <julian.declercq1@gmail.com>
2023-04-07 01:40:13 +0200

    co shortcut - added options

--- co shortcut/main.lua
+++ co shortcut/main.lua
@@ -3,12 +3,38 @@ meta.version = "0.1"
 meta.description = "Sandbox"
 meta.author = "Jools"
 
+backwear = {
+  { ENT_TYPE.ITEM_JETPACK,             "Jetpack" },
+  { ENT_TYPE.ITEM_VLADS_CAPE,          "Vlad's Cape" },
+  { 0,                                 "-----------" },
+  { ENT_TYPE.ITEM_CAPE,                "Cape" },
+  { ENT_TYPE.ITEM_HOVERPACK,           "Hoverpack" },
+  { ENT_TYPE.ITEM_TELEPORTER_BACKPACK, "Telepack" },
+  { ENT_TYPE.ITEM_POWERPACK,           "Powerpack" },
+}
+
+local comboOptions = ""
+for _, item in pairs(backwear) do
+  comboOptions = comboOptions .. item[2] .. "\0"
+end
+register_option_combo("backwear", "Backwear", "", comboOptions .. "\0", 3)
+
+register_option_bool("kapala", "Kapala", "", true)
+register_option_bool("spike_shoes", "Spike Shoes", "", true)
+register_option_bool("ankh", "Ankh", "", true)
+register_option_bool("alien_compass", "Alien Compass", "", true)
+register_option_bool("eggplant_crown", "Eggplant Crown", "", true)
+register_option_bool("climbing_gloves", "Climbing Gloves", "", false)
+register_option_bool("true_crown", "True Crown", "", false)
+register_option_bool("pitchers_mitt", "Pitcher's Mitt", "", false)
+register_option_bool("bomb_paste", "Bomb Paste", "", false)
+register_option_bool("spring_shoes", "Spring Shoes", "", false)
+
 local spawnPortalHitbox = nil
 local playerEntType = nil
 set_callback(function()
   spawnPortalHitbox = AABB:new():offset(47, 109):extrude(5)
   playerEntType = players[1].type.id
-  god(true)
 end, ON.CAMP)
 
 set_callback(function()
@@ -32,7 +58,6 @@ set_callback(function()
       -- reset the hitbox to avoid collision detection every frame until ON.CAMP happens again
       spawnPortalHitbox = nil
     end
-    print("spawned portal")
   end
 end, ON.GAMEFRAME)
 
@@ -47,13 +72,24 @@ set_callback(function()
   end
 
   if state.world_next == 7 and state.level_next == 5 then
+    print(F 'backwear on index {inspect(backwear[options.backwear])}')
+
     local player = get_player(1, false)
-    player:give_powerup(ENT_TYPE.ITEM_POWERUP_KAPALA)
-    player:give_powerup(ENT_TYPE.ITEM_POWERUP_SPIKE_SHOES)
-    player:give_powerup(ENT_TYPE.ITEM_POWERUP_ANKH)
-    player:give_powerup(ENT_TYPE.ITEM_POWERUP_SPECIALCOMPASS)
-    player:give_powerup(ENT_TYPE.ITEM_POWERUP_EGGPLANTCROWN)
-    pick_up(players[1].uid, spawn(ENT_TYPE.ITEM_JETPACK, 0, 0, LAYER.PLAYER, 0, 0))
+    if options.kapala then player:give_powerup(ENT_TYPE.ITEM_POWERUP_KAPALA) end
+    if options.spike_shoes then player:give_powerup(ENT_TYPE.ITEM_POWERUP_SPIKE_SHOES) end
+    if options.ankh then player:give_powerup(ENT_TYPE.ITEM_POWERUP_ANKH) end
+    if options.alien_compass then player:give_powerup(ENT_TYPE.ITEM_POWERUP_SPECIALCOMPASS) end
+    if options.eggplant_crown then player:give_powerup(ENT_TYPE.ITEM_POWERUP_EGGPLANTCROWN) end
+    if options.climbing_gloves then player:give_powerup(ENT_TYPE.ITEM_POWERUP_CLIMBING_GLOVES) end
+    if options.true_crown then player:give_powerup(ENT_TYPE.ITEM_POWERUP_TRUECROWN) end
+    if options.pitchers_mitt then player:give_powerup(ENT_TYPE.ITEM_POWERUP_PITCHERSMITT) end
+    if options.bomb_paste then player:give_powerup(ENT_TYPE.ITEM_POWERUP_PASTE) end
+    if options.spring_shoes then player:give_powerup(ENT_TYPE.ITEM_POWERUP_SPRING_SHOES) end
+
+    local selectedBackwear = backwear[options.backwear][1]
+    if selectedBackwear > 0 then
+      pick_up(player.uid, spawn(selectedBackwear, 0, 0, LAYER.PLAYER, 0, 0))
+    end
   end
   levelCounter = levelCounter + 1
 end, ON.LEVEL)
