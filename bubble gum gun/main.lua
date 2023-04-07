eb1fe8cf677df64c7ec001055c15d4632893df40
Julian Declercq <julian.declercq1@gmail.com>
2023-04-08 00:47:39 +0200

    bubble gum gun - colours and prevent when on mount, changed cooldown too

--- bubble gum gun/main.lua
+++ bubble gum gun/main.lua
@@ -1,10 +1,12 @@
--- set_pre_entity_spawn(function(type, x, y, l, overlay)
---   print(F 'type in callback {type}')
---   return spawn_entity(ENT_TYPE.ITEM_AXOLOTL_BUBBLESHOT, x, y, l, 0, 0)
--- end, SPAWN_TYPE.SYSTEMIC, 0, ENT_TYPE.ITEM_BULLET, ENT_TYPE.ITEM_FIREBALL)
+meta.name = "Julle Development"
+meta.version = "0.1"
+meta.description = "Sandbox"
+meta.author = "Jools"
+
+local bubbleGumColor = { r = 255, g = 105, b = 180, a = 255 }
 
 local currentCooldown = 0
-local maxCooldown = 16 -- every 60 frames you can fire
+local maxCooldown = 24 -- X frames cooldown between 2 bubbles
 local heldLastFrame = false
 local hasReleasedSinceLast = false
 set_callback(function()
@@ -12,23 +14,27 @@ set_callback(function()
   local input = player.input.buttons_gameplay
   local holdCurrentFrame = test_flag(input, INPUT_FLAG.WHIP)
 
+  local mounted = player:topmost_mount().uid ~= player.uid
+
   local canWhip = player.state ~= CHAR_STATE.DUCKING and
       player.state ~= CHAR_STATE.THROWING and
-      player.holding_uid < 0
+      player.state ~= CHAR_STATE.SITTING and
+      player.holding_uid < 0 and
+      not mounted
 
-  --print(F 'last: {heldLastFrame}, current: {holdCurrentFrame}, cd {currentCooldown}')
   if holdCurrentFrame and canWhip then
     if hasReleasedSinceLast and currentCooldown <= 0 then
-      print("spawning!")
       local facingLeft = player.rendering_info.facing_left
       local direction = 1
       if facingLeft then
         direction = -1
       end
 
-      local spawned = get_entity(spawn_critical(ENT_TYPE.ITEM_FREEZERAYSHOT, player.x + 1 * direction, player.y,
+      print("spawning bubble!")
+      local spawned = get_entity(spawn_critical(ENT_TYPE.ITEM_AXOLOTL_BUBBLESHOT, player.x + 1 * direction, player.y,
         LAYER.FRONT, 0, 0))
       spawned.velocityx = 0.5 * direction
+      spawned.color:set_rgba(bubbleGumColor.r, bubbleGumColor.g, bubbleGumColor.b, bubbleGumColor.a)
 
       spawned.owner_uid = player.uid
 
@@ -46,3 +52,22 @@ set_callback(function()
 
   heldLastFrame = holdCurrentFrame
 end, ON.FRAME)
+
+local toRecolor = {
+  PARTICLEEMITTER.AXOLOTL_SMALLBUBBLEKILL,
+  PARTICLEEMITTER.AXOLOTL_BIGBUBBLEKILL
+}
+
+set_callback(function()
+  for i = 1, #toRecolor do
+    local particle = get_particle_type(toRecolor[i])
+    particle.red = bubbleGumColor.r
+    particle.green = bubbleGumColor.g
+    particle.blue = bubbleGumColor.b
+  end
+end, ON.START)
+
+-- Color normal axolotls bubbles too :)
+set_post_entity_spawn(function(bubble)
+  bubble.color:set_rgba(bubbleGumColor.r, bubbleGumColor.g, bubbleGumColor.b, bubbleGumColor.a)
+end, SPAWN_TYPE.SYSTEMIC, MASK.ITEM, ENT_TYPE.ITEM_AXOLOTL_BUBBLESHOT)
