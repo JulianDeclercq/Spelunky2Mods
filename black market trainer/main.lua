4461a002cce916050b374f12bf21badbd108eda7
Julian Declercq <julian.declercq1@gmail.com>
2023-04-15 19:07:05 +0200

    black market trainer - refactor and fixed infinite warp bug

--- black market trainer/main.lua
+++ black market trainer/main.lua
@@ -1,7 +1,7 @@
-meta.name = "__Julle dev"
+meta.name = "Black Market Trainer"
 meta.version = "1.0"
 meta.description =
-""
+"Practise robbing the black market. Offers a variaty of items to start with and displays your shopkeeper aggro number."
 meta.author = "Jools"
 
 local WORLD_JUNGLE = 2
@@ -9,22 +9,6 @@ local firstPossibleBlackMarketLevel = 2
 local currentLevel = firstPossibleBlackMarketLevel
 
 local blackMarket = { x = 30, y = 86 }
-
-local orderPrefix = 1
-local optionNameLookup = {}
-function OrderedName(name)
-    -- return existing
-    local existing = optionNameLookup[name]
-    if existing ~= nil then
-        return existing
-    end
-
-    -- add new
-    local orderedName = string.format("%03i_%s", orderPrefix, name)
-    optionNameLookup[name] = orderedName
-    orderPrefix = orderPrefix + 1
-    return orderedName
-end
 
 local backwear <const> = {
     { ent_type = 0,                                 name = "None" },
@@ -50,19 +34,35 @@ local heldItems <const> = {
     { ent_type = ENT_TYPE.ITEM_HOUYIBOW,     name = "Hou Yi's Bow" },
 }
 
-function RedWhiteLerp(t)
-    local from = { r = 139, g = 0, b = 0 }   -- red
-    local to = { r = 255, g = 255, b = 255 } -- white
+local orderPrefix = 1
+local optionNameLookup = {}
+function OrderedName(name)
+    -- return existing
+    local existing = optionNameLookup[name]
+    if existing ~= nil then
+        return existing
+    end
 
-    local r = math.floor(from.r + (to.r - from.r) * t)
-    local g = math.floor(from.g + (to.g - from.g) * t)
-    local b = math.floor(from.b + (to.b - from.b) * t)
-
-    return { r = r, g = g, b = b, a = 255 }
+    -- add new
+    local orderedName = string.format("%03i_%s", orderPrefix, name)
+    optionNameLookup[name] = orderedName
+    orderPrefix = orderPrefix + 1
+    return orderedName
 end
 
 function Lerp(a, b, t)
     return a + (b - a) * t
+end
+
+function RedWhiteLerp(t)
+    local from = { r = 139, g = 0, b = 0 }   -- red
+    local to = { r = 255, g = 255, b = 255 } -- white
+
+    local r = math.floor(Lerp(from.r, to.r, t))
+    local g = math.floor(Lerp(from.g, to.g, t))
+    local b = math.floor(Lerp(from.b, to.b, t))
+
+    return { r = r, g = g, b = b, a = 255 }
 end
 
 function GenerateOptions(input)
@@ -73,8 +73,8 @@ function GenerateOptions(input)
     return options .. "\0"
 end
 
-register_option_combo(OrderedName("backwear"), "Backwear", "", GenerateOptions(backwear), 2)
-register_option_combo(OrderedName("held_item"), "Held item", "", GenerateOptions(heldItems), 2)
+register_option_combo(OrderedName("backwear"), "Backwear", "", GenerateOptions(backwear), 1)
+register_option_combo(OrderedName("held_item"), "Held item", "", GenerateOptions(heldItems), 4)
 
 register_option_int(OrderedName("money"), "Money", "", 20000, 0, 1000000)
 register_option_int(OrderedName("bombs"), "Bombs", "", 4, 0, 99)
@@ -82,21 +82,6 @@ register_option_int(OrderedName("ropes"), "Ropes", "", 4, 0, 99)
 register_option_bool(OrderedName("bomb_paste"), "Bomb Paste", "", false)
 register_option_bool(OrderedName("pitchers_mitt"), "Pitcher's Mitt", "", false)
 register_option_bool(OrderedName("climbing_gloves"), "Climbing Gloves", "", false)
-
-set_callback(function()
-    -- on going through the main door in camp
-    if state.loading == 1 and state.screen_next == ON.LEVEL
-        and state.world_next == 1 and state.level_next == 1 and state.theme_next == THEME.DWELLING then
-        state.world_next = WORLD_JUNGLE
-        state.level_next = firstPossibleBlackMarketLevel
-        state.theme_next = THEME.JUNGLE
-
-        -- for instant restart
-        state.world_start = WORLD_JUNGLE
-        state.level_start = firstPossibleBlackMarketLevel
-        state.theme_start = THEME.JUNGLE
-    end
-end, ON.LOADING)
 
 function GearPlayer()
     local player = get_player(1, false)
@@ -127,8 +112,9 @@ function GearPlayer()
     end
 end
 
+local visitedBlackMarket = false
 set_callback(function()
-    if state.world ~= WORLD_JUNGLE or state.theme ~= THEME.JUNGLE then
+    if state.world ~= WORLD_JUNGLE or state.theme ~= THEME.JUNGLE or visitedBlackMarket then
         return
     end
 
@@ -140,6 +126,7 @@ set_callback(function()
         local player = get_player(1, false)
         player:set_layer(LAYER.BACK)
         player:set_position(blackMarket.x, blackMarket.y)
+        visitedBlackMarket = true
     else
         -- warp to the next level and check for the black market there
         currentLevel = currentLevel + 1
@@ -147,49 +134,62 @@ set_callback(function()
     end
 end, ON.LEVEL)
 
+set_callback(function()
+    -- on going through the main door in camp
+    if state.loading == 1 and state.screen_next == ON.LEVEL
+        and state.world_next == 1 and state.level_next == 1 and state.theme_next == THEME.DWELLING then
+        state.world_next = WORLD_JUNGLE
+        state.level_next = firstPossibleBlackMarketLevel
+        state.theme_next = THEME.JUNGLE
+
+        -- for instant restart
+        state.world_start = WORLD_JUNGLE
+        state.level_start = firstPossibleBlackMarketLevel
+        state.theme_start = THEME.JUNGLE
+    end
+end, ON.LOADING)
+
 local lastAggro = 0
 local currentAggro = 0
-local drawBiggerDurationInSeconds = 2
+local animationDurationInSeconds = 2
 local framesToDrawBigger = 0.0
-local framesToDrawBiggerStart = 0.0 -- used to calculate progression in animation
-local animationProgression = 0.0
+local framesToDrawBiggerStart = 0.0 -- used to calculate animation progress
+local animationProgress = 0.0
 set_callback(function(draw_ctx)
     local color = { r = 255, g = 255, b = 255, a = 255 } -- white
     local size = 25
 
     if framesToDrawBigger > 0 then
-        color = RedWhiteLerp(animationProgression)
-        size = Lerp(35, 25, animationProgression)
+        color = RedWhiteLerp(animationProgress)
+        size = Lerp(35, 25, animationProgress)
     end
 
     local text = F 'Shop keeper aggro {state.shoppie_aggro_next}'
     local w, h = draw_text_size(size, text)
     draw_ctx:draw_text(1 - w, -1 - h, size, text, rgba(color.r, color.g, color.b, color.a)) -- right bottom corner
-    --draw_ctx:draw_text(1 - w, -1 - h, size, text, Color:lime():get_ucolor()) -- right bottom corner
 end, ON.GUIFRAME)
 
 set_callback(function()
     local engineFps = 60
     currentAggro = state.shoppie_aggro_next
     if currentAggro > lastAggro then
-        framesToDrawBigger = drawBiggerDurationInSeconds * engineFps
+        framesToDrawBigger = animationDurationInSeconds * engineFps
         framesToDrawBiggerStart = framesToDrawBigger
         lastAggro = currentAggro
     end
 
     if framesToDrawBigger > 0 then
         framesToDrawBigger = framesToDrawBigger - 1
-        animationProgression = 1 - (framesToDrawBigger / framesToDrawBiggerStart)
-    else --reset animation progression
-        --animationProgression = 0
+        animationProgress = 1 - (framesToDrawBigger / framesToDrawBiggerStart)
     end
 end, ON.GAMEFRAME)
 
 set_callback(function()
     currentLevel = firstPossibleBlackMarketLevel
+    visitedBlackMarket = false
     lastAggro = 0
     currentAggro = 0
     framesToDrawBigger = 0.0
     framesToDrawBiggerStart = 0.0
-    animationProgression = 0.0
+    animationProgress = 0.0
 end, ON.RESET)
